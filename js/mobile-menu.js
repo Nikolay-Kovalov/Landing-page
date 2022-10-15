4fe43491dddb89d879ad0fe03d9d6e4f4888a7f6
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2022-10-15 15:04:59 +0300

    Edit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,6 +4,8 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const closeMenu = document.querySelector('.js-close');
+   const closeMen = document.querySelector('.js-clos');
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -19,6 +21,8 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+  closeMenu.addEventListener('click', toggleMenu);
+    closeMen.addEventListener('click', toggleMenu);
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
