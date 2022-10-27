b16fe629502f7be04e036bc3d4e378d667a93065
Nikolay-Kovalov <lawyerpartners.ua@gmail.com>
2022-10-27 09:56:01 +0300

    Edit js

---
+++ js/js-counter.js
@@ -0,0 +1,22 @@
+const btnDecrRef = document.querySelector('button[data-action="decrement"]');
+const btnIncrRef = document.querySelector('button[data-action="increment"]');
+const spanRef = document.querySelector('#value');
+const priceRef = document.querySelector('.thumb__price')
+
+let counterValue = 1;
+btnDecrRef.addEventListener('click', decrement);
+btnIncrRef.addEventListener('click', increment);
+
+function decrement(evt) {
+    if (spanRef.textContent = counterValue -= 1 && counterValue >=1) {
+        priceRef.textContent -= 400 
+    }
+return ; 
+};
+
+function increment(evt) {
+    if (spanRef.textContent = counterValue += 1 ) {
+        priceRef.textContent = 400 * counterValue
+    }
+return ; 
+};
